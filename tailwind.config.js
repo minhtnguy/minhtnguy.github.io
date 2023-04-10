f240513691e1ae059bc33d04c4b370fce3aed784
mtnguy16 <mtnguy16@uci.edu>
2023-04-09 21:09:18 -0700

    added case study

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       colors: {
         'teal': '#006794',
         'pink': '#E0838E',
-        'black': '#2E2E2E'
+        'black': '#2E2E2E',
+        // 'half-court-sports-pink': '#F4CDD4'
       }
     },
   },
