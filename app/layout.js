fb6c11a0e0d8529c493d9422ca75f5c8f4290573
Minh Nguyen <76888415+minhtnguy@users.noreply.github.com>
2025-03-03 21:07:45 -0500

    clarity

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,14 @@
+"use client";
+
 import { Inter } from "next/font/google";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { Metadata } from "next";
 import "./globals.css";
-
+import Clarity from "@microsoft/clarity";
+import { useEffect } from "react";
 const inter = Inter({ subsets: ["latin"] });
+import ClarityAnalytics from "./components/ClarityAnalytics";
 
 export const metadata = {
 	title: "Minh Nguyen",
@@ -13,6 +17,9 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+	useEffect(() => {
+		Clarity.init("qiq5nhksuj");
+	}, []);
 	return (
 		<html lang="en" className={GeistSans.className}>
 			<body>{children}</body>
