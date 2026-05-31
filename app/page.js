c0eadcd2c0c58e1318b59cd8cf229e2e8f33eeba
Minh Nguyen <76888415+minhtnguy@users.noreply.github.com>
2026-05-31 01:22:26 -0400

    view transition home

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 "use client";
 
 import StickerStack from "./components/StickerStack";
+import ViewTransition from "@/app/components/ViewTransition";
 
 export default function Home() {
 	return (
-		<div className="bg-[#FFFFFF] relative z-10 overflow-visible">
-			<StickerStack />
-		</div>
+		<ViewTransition>
+			<div className="bg-[#FFFFFF] relative z-10 overflow-visible">
+				<StickerStack />
+			</div>
+		</ViewTransition>
 	);
 }
